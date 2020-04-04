e059b0ea91aae03182ac2d546e11850ac342c570
JonasJore <jonas.jore@gmail.com>
2020-04-04 23:10:12 +0200

    added plugin for sass

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,6 +5,7 @@ module.exports = {
       options: {
         tslint: false
       }
-    }
+    },
+    `gatsby-plugin-sass`
   ]
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,8 @@
 import React from "react"
 import { Link } from "gatsby"
 
+import "../styles/main.scss"
+
 export default (): JSX.Element => (
   <div>
     Hello world!<br/>
